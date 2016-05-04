ffeed24f100affdde0216ed5df103aa5b5877767
Nikhil D'Souza <nikhiljay7@gmail.com>
2016-05-03 23:01:58 -0700

    Added sample ResearchKit/HealthKit app files.

--- Curvature/Curvature/AppDelegate.swift
+++ Curvature/Curvature/AppDelegate.swift
@@ -7,40 +7,74 @@
 //
 
 import UIKit
+import ResearchKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    let healthStore = HKHealthStore()
+    
     var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    var containerViewController: ResearchContainerViewController? {
+        return window?.rootViewController as? ResearchContainerViewController
+    }
+    
+    func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        let standardDefaults = NSUserDefaults.standardUserDefaults()
+        if standardDefaults.objectForKey("ORKSampleFirstRun") == nil {
+            ORKPasscodeViewController.removePasscodeFromKeychain()
+            standardDefaults.setValue("ORKSampleFirstRun", forKey: "ORKSampleFirstRun")
+        }
+        
+        // Appearance customization
+        let pageControlAppearance = UIPageControl.appearance()
+        pageControlAppearance.pageIndicatorTintColor = UIColor.lightGrayColor()
+        pageControlAppearance.currentPageIndicatorTintColor = UIColor.blackColor()
+        
+        // Dependency injection.
+        containerViewController?.injectHealthStore(healthStore)
         return true
     }
-
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        lockApp()
+        return true
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        if ORKPasscodeViewController.isPasscodeStoredInKeychain() {
+            // Hide content so it doesn't appear in the app switcher.
+            containerViewController?.contentHidden = true
+        }
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        lockApp()
     }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    func lockApp() {
+        /*
+         Only lock the app if there is a stored passcode and a passcode
+         controller isn't already being shown.
+         */
+        guard ORKPasscodeViewController.isPasscodeStoredInKeychain() && !(containerViewController?.presentedViewController is ORKPasscodeViewController) else { return }
+        
+        window?.makeKeyAndVisible()
+        
+        let passcodeViewController = ORKPasscodeViewController.passcodeAuthenticationViewControllerWithText("Welcome back to ResearchKit Sample App", delegate: self) as! ORKPasscodeViewController
+        containerViewController?.presentViewController(passcodeViewController, animated: false, completion: nil)
     }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
+
+extension AppDelegate: ORKPasscodeDelegate {
+    func passcodeViewControllerDidFinishWithSuccess(viewController: UIViewController) {
+        containerViewController?.contentHidden = false
+        viewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func passcodeViewControllerDidFailAuthentication(viewController: UIViewController) {
+    }
+}
+
 
